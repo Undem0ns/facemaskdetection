6f215c78cadadfc8eeaf35fc6ad2e727af1087e6
Undem0ns <andamannira@kkumail.com>
2021-04-25 00:13:54 +0700

    upgrade-to-flutter-2.0.5

--- lib/route.dart
+++ lib/route.dart
@@ -1,11 +1,6 @@
-import 'package:camera/camera.dart';
-import 'package:facemaskdetection/main.dart';
 import 'package:facemaskdetection/widget/detect_image.dart';
-import 'package:facemaskdetection/widget/detect_on_camera.dart';
 import 'package:flutter/material.dart';
 
 final Map<String, WidgetBuilder> routes = {
   '/detectimage': (BuildContext context) => DetectImage(),
-  // '/detectoncamera': (BuildContext context) =>
-  //     DetectOnCamera(MyApp().getCamera()),
 };
